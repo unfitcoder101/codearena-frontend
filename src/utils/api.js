a8e502d37e253356f09eb3bde6bfd9638c492c76
Harshvardhan <hvkasli007@gmail.com>
2026-05-12 23:31:49 +0530

    fix api url for production

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,4 @@
 // This is the single place your API URL lives
 // Change this one line when you deploy to production
 const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:4000";
-
 export default API_BASE;
